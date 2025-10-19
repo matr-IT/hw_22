643dc428fdb49e0d64bc4baae498d76933d2dfac
rybin <rybin.32@gmail.com>
2025-10-19 22:36:28 +0300

    hw 24 is done

--- catalog/management/commands/add_product.py
+++ catalog/management/commands/add_product.py
@@ -3,22 +3,45 @@ from unicodedata import category
 from django.core.management.base import BaseCommand
 from catalog.models import Category, Product
 
+
 class Command(BaseCommand):
-    help = 'Add products to database'
+    help = "Add products to database"
 
     def handle(self, *args, **options):
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-        category, _ = Category.objects.get_or_create(name='cat_1', description='123')
+        category, _ = Category.objects.get_or_create(
+            name="smartphones", description="smart telephones with full-touch displays"
+        )
         products = [
-            {'name': 'iphone', 'description': 123, 'category': category, 'price': 100},
-            {'name': 'samsung', 'description': 321, 'category': category, 'price': 80}
+            {
+                "name": "iphone",
+                "description": "A smartphone from Apple",
+                "category": category,
+                "price": 100000,
+            },
+            {
+                "name": "samsung",
+                "description": "A smartphone not from Apple",
+                "category": category,
+                "price": 80000,
+            },
+            {
+                "name": "xiaomi",
+                "description": "A smartphone also not from Apple",
+                "category": category,
+                "price": 60000,
+            },
         ]
 
         for p in products:
             product, created = Product.objects.get_or_create(**p)
             if created:
-                self.stdout.write(self.style.SUCCESS(f'Успешно добавлен продукт:{product.name}'))
+                self.stdout.write(
+                    self.style.SUCCESS(f"Успешно добавлен продукт:{product.name}")
+                )
             else:
-                self.stdout.write(self.style.WARNING(f'Продукт уже существует:{product.name}'))
+                self.stdout.write(
+                    self.style.WARNING(f"Продукт уже существует:{product.name}")
+                )
