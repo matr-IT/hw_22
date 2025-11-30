13f17b3390a43f4ca0ec95fea875d3f547a922f3
rybin <rybin.32@gmail.com>
2025-11-30 22:00:51 +0300

    HW 29 is done

--- catalog/models.py
+++ catalog/models.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.db import models
-from django.db.models import BooleanField, CASCADE
+from django.db.models import BooleanField
 
-from users.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="название")
     description = models.TextField(verbose_name="описание", blank=True)
+
 
     def __str__(self):
         return self.name
